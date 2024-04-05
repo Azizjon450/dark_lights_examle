78734904fe9e9d131fb7dd8efba2b6d66b978b10
Azizjon <qodirovazizjon.natura@gmail.com>
2024-04-05 16:54:41 +0500

    dark lights

---
+++ android/app/src/main/kotlin/com/example/dark_lights_examle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dark_lights_examle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:dark_lights_examle/components/box.dart';
+import 'package:dark_lights_examle/components/button.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: MyBox(
+          color: Theme.of(context).colorScheme.primary,
+          child: MyButton(
+            color: Theme.of(context).colorScheme.secondary,
+            onTap: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
